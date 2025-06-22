73ad043586ba6b20475c65ced377b873d5d0b278
Mudasir Jamshaid <thisismjeyz@gmail.com>
2025-06-22 06:28:47 +0500

    refactor: update Flask app to use Bootstrap5 and add visitor table in PostgreSQL

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from flask import Flask, render_template, send_from_directory, request
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap5
 import smtplib
 import dotenv
 import os
+import psycopg2
+
 
 app = Flask(__name__)
-Bootstrap(app)
+Bootstrap5(app)
 dotenv.load_dotenv()
+app.config["SECRET_KEY"] = "cvdsg3456w7w3456gh"
+DATABASE_URL = "postgresql://postgres:9992@localhost:5432/portfolio"
+
+conn = psycopg2.connect(DATABASE_URL)
+
+
+with conn.cursor() as cursor:
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS visitor (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(150) NOT NULL,
+            email VARCHAR(150) UNIQUE NOT NULL,
+            subject VARCHAR(150) NOT NULL,
+            message VARCHAR(150) NOT NULL
+        )
+    """)
+    conn.commit()
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -23,6 +42,12 @@ def home():
             server.login(os.getenv("YOUR_GMAIL"), os.getenv("YOUR_PASSWORD"))
             email_message = f"Subject: {subject}\n\nName: {name}\nEmail: {email}\nMessage: {message}"
             server.sendmail(to_addrs=os.getenv("YOUR_GMAIL"), from_addr=os.getenv('YOUR_GMAIL'), msg=email_message)
+
+
+        with conn.cursor() as cur:
+            cur.execute("INSERT INTO visitor (name, email, subject, message) VALUES (%s, %s, %s, %s)",
+                        (name, email, subject, message))
+            conn.commit()
 
     return render_template("index.html")
 
